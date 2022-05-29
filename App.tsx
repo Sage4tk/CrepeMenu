e1846a68912da63cbd4230b878ebab8be9c1001a
timothy timbol <ttimbol08@gmail.com>
2022-05-29 16:13:13 +0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Very nice</Text>
+      <TouchableOpacity style={styles.buttleStyle} onPress={() => {alert("TEST")}}>
+        <Text style={styles.insideBtn}>Button</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textShadowColor: '#585858',
+    textShadowOffset: {width:5,height:5},
+    textShadowRadius: 10
   },
+  buttleStyle: {
+    backgroundColor: "#764ABC",
+    padding: 7
+  },
+  insideBtn: {
+    color: '#fff'
+  }
 });
